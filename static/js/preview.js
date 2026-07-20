175864ca829352ad9d768c497a7ce2ae3f6b9386
kumar-shanu-1881 <kumarshanu1881@gmail.com>
2026-07-20 22:11:58 +0530

    added js file to strem camera

---
+++ static/js/preview.js
@@ -0,0 +1,82 @@
+const video = document.querySelector("#webcam");
+const canvas = document.querySelector("#canvas");
+
+// Start Webcam
+navigator.mediaDevices.getUserMedia({
+    video: true
+})
+.then(stream => {
+    video.srcObject = stream;
+    // setInterval(detectFace, 150); // Call detectFace every 150ms
+    video.onloadedmetadata = () => {
+        detectFace(); // Start the loop manually once
+    };
+})
+.catch(err => {
+    alert("Access to Camera Denied.");
+    console.log(err);
+});
+
+//sending frames to the server for face detection
+async function detectFace(){
+    if(detectionStopped)
+        return;
+
+    if(video.videoWidth===0) return;
+    
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    const ctx = canvas.getContext("2d");
+
+    ctx.drawImage(video,0,0,canvas.width,canvas.height);
+
+    canvas.toBlob(async (blob) => {
+
+         latestBlob = blob;
+        const formData = new FormData();
+        formData.append("frame", blob);
+        
+        try {
+            
+            const response = await fetch("/detect", {
+                method: "POST",
+                body: formData,
+            });
+            
+            const data = await response.json();
+
+            faceDetected = data.face
+
+            if (data.success) {
+                faceDetected = data.face;
+                registerBtn.disabled = false;
+                statusMsg.style.color = "lightgreen";
+                statusMsg.innerHTML = "✅ " + data.message;
+                
+            } else{
+                faceDetected = false;
+
+                registerBtn.disabled = true;
+                registerBtn.style.color="00072D"
+                
+                statusMsg.style.color = "red";
+
+                statusMsg.innerHTML = "❌ " + data.message;
+
+            }
+
+            } catch (err) {
+
+            console.log(err);
+
+        }
+        finally {
+            // CRITICAL: Schedule the next frame ONLY after this one finishes (or fails)
+            // This prevents network queue flooding
+            setTimeout(detectFace, 350);
+        }
+
+    }, "image/jpeg");
+
+}
