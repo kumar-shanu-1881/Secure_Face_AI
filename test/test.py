38827f5e9d9f17b4f1d5eee62e0d2127940c99fd
kumar-shanu-1881 <kumarshanu1881@gmail.com>
2026-07-21 22:26:37 +0530

    changed to test some file

--- test/test.py
+++ test/test.py
@@ -1,14 +1,30 @@
-from app.db.mongodb import users
+# from app.db.mongodb import users
 
-# users.insert_one({
-#     "name": "Kumar Shanu",
-#     "email": "kumar@examhai.com"
-# })
-from app.db.user_repo import user_repo
+# # users.insert_one({
+# #     "name": "Kumar Shanu",
+# #     "email": "kumar@examhai.com"
+# # })
+# from app.db.user_repo import user_repo
 
-# data=user_repo.get_user_by_user_id("KUMA-1DC235")
-# print("User data:- ",data)
+# # data=user_repo.get_user_by_user_id("KUMA-1DC235")
+# # print("User data:- ",data)
 
-resp=user_repo.get_user_by_email("kumarshanu1881@gmail.com")
-print("User resp:- ",resp)
-print("Connected successfully!")
+# resp=user_repo.get_user_by_email("kumarshanu1881@gmail.com")
+# print("User resp:- ",resp)
+# print("Connected successfully!")
+
+from pathlib import Path
+import cv2
+
+img = Path(
+r"C:\Users\bnsah\OneDrive\文档\PROJECTS(FINAL)\SecureFace AI\test\evaluation\dataset\celeba_subset\000023.jpg"
+)
+
+print(img.exists())
+
+image = cv2.imread(str(img))
+
+print(image)
+
+if image is not None:
+    print(image.shape)
