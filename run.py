3566c44c978fd331372be3429859e95427fa6df4
kumar-shanu-1881 <kumarshanu1881@gmail.com>
2026-07-25 20:23:48 +0530

    add gunicorn in to run multiple user at once

--- run.py
+++ run.py
@@ -32,6 +32,6 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
-    # port = int(os.environ.get("PORT", 7860))
-    # app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
